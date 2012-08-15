4f9e05d459a225b90aae1d48fd3e7fd43cae0008
Deleterios <deleterios@gmail.com>
2012-08-15 17:14:15 +0200

    unexpand and apply new standards (here for if/function/switch/while)
    Signed-off-by: Deleterios <deleterios@gmail.com>

--- pub/inc/template/footer.php
+++ pub/inc/template/footer.php
@@ -3,16 +3,20 @@
 <p>powered by <a href="http://zirafe.github.com">Zirafe</a></p>
 </div>
 <script type="text/javascript">
- $(document).ready(function(){
-    $("input").click(function(event){
-         this.select();
-            });
-    $("p.options").click(function(event){
-         $("p.options").hide();
-	 $("#upload").hide();
-         $(".hide").show();
-            });
-             });
+$(document).ready(function()
+{
+	$("input").click(function(event)
+	{
+		this.select();
+	});
+
+	$("p.options").click(function(event)
+	{
+		$("p.options").hide();
+	 	$("#upload").hide();
+		$(".hide").show();
+	});
+});
 </script>
 </body>
 </html>
